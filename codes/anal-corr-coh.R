d5601d828ae441f4de2f67f90321d6970fedcc34
Michal <michal@mmac.predsci.com>
2017-02-03 10:29:23 -0800

    mbn: updated the anal-corr-coh.R code and the figures it makes

--- codes/anal-corr-coh.R
+++ codes/anal-corr-coh.R
@@ -49,21 +49,18 @@ setwd(dirName)
 file_list <- list.files(pattern=".csv")
 
 cat("\n\nLoading Results for Figures 3, 4, 5a and 6a\n")
-for (file in file_list){
-       
-  # if the merged dataset doesn't exist, create it
-  if (!exists("dataset")){
-    dataset <- read.csv(file, header=TRUE)
-  }
-   
-  # if the merged dataset does exist, append to it
-  if (exists("dataset")){
-    temp_dataset <-read.csv(file, header=TRUE)
-    dataset<-rbind(dataset, temp_dataset)
-    rm(temp_dataset)
-  }
- 
+
+for (file in file_list) {
+	# if the merged dataset doesn't exist, create it
+	if (!exists("dataset")) {
+		dataset <- read.csv(file, header = TRUE)
+	} else { # if the merged dataset does exist, append to it
+		temp_dataset <- read.csv(file, header = TRUE)
+		dataset <- rbind(dataset, temp_dataset)
+		rm(temp_dataset)
+	}
 }
+
 
 ## Set working directory back to working directory
 
